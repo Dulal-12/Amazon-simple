61450205f63ef226e60bc1ed65aefd475c569e2a
Md.Dulal Miah <dulalmiah70112@gmail.com>
2021-01-03 21:55:00 +0600

    done and fixed some issu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from '../../images/logo.png';
 import './Header.css';
-import { a  } from 'react-router-dom';
+import { Link  } from 'react-router-dom';
 
 const Header = () => {
 
@@ -14,7 +14,7 @@ const Header = () => {
             <nav>
                 <a href="/shop">Shop</a >
                 <a href="/review">Order Review</a >
-                <a href="/manage">Mange Inventory</a >
+                <a href="/manage">MLinknge Inventory</a >
             </nav>
             
         </div>
